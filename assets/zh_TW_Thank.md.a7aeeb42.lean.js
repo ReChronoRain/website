049e55d3ad2820b34db1820d848307f1be876ab6
liuran001 <liuran001@users.noreply.github.com>
2023-05-23 13:51:59 +0000

    deploy: fdea44ec7731416d0f80771df7915a8014fb24d5

---
+++ assets/zh_TW_Thank.md.a7aeeb42.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c as t,O as i}from"./chunks/framework.4afe7240.js";const p=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"zh_TW/Thank.md","filePath":"zh_TW/Thank.md"}'),a={name:"zh_TW/Thank.md"},o=i("",3),l=[o];function n(h,s,b,u,f,g){return r(),t("div",null,l)}const _=e(a,[["render",n]]);export{p as __pageData,_ as default};
