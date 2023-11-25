0c1981b31d48dcac4ce403b3a357a19b5cfa63f7
igor <134963561+igormiguell@users.noreply.github.com>
2023-11-24 21:07:17 -0300

    Update pt_BR.ts

--- .vitepress/locales/pt_BR.ts
+++ .vitepress/locales/pt_BR.ts
@@ -19,8 +19,8 @@ function nav() {
         { text: 'Download', link: '/pt_BR/Download.html' },
         { text: 'Doar', link: 'https://www.sevtinge.cc/t/donation.html' },
         { text: 'Obrigado', link: '/pt_BR/Thank.html' },
-        { text: 'Comunicação', link: '/pt_BR/Support.html' },
-        { text: 'Protocolo', link: '/pt_BR/Protocol.html' },
-        { text: 'Privacidade', link: '/pt_BR/Privacy.html' }
+        { text: 'Suporte', link: '/pt_BR/Support.html' },
+        { text: 'Acordo do usuário', link: '/pt_BR/Protocol.html' },
+        { text: 'Política de privacidade', link: '/pt_BR/Privacy.html' }
     ]
 }
