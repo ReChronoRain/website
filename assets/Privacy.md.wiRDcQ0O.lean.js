a059bcf7de752502ddf333f2013b4c837b1d02f1
Sevtinge <Sevtinge@users.noreply.github.com>
2024-12-03 12:01:50 +0000

    deploy: 91b0ced0990f95a6e613a6ba9dc48dc3e765c23e

---
+++ assets/Privacy.md.wiRDcQ0O.lean.js
@@ -0,0 +1,1 @@
+import{_ as l,c as i,o as e,a2 as a}from"./chunks/framework.BvCKZEGt.js";const p=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"Privacy.md","filePath":"Privacy.md"}'),t={name:"Privacy.md"},r=a("",15),o=[r];function n(h,u,c,s,d,_){return e(),i("div",null,o)}const m=l(t,[["render",n]]);export{p as __pageData,m as default};
